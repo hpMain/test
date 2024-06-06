ef8054a00500aac5838256d54736484cf8055b66
Hosting Provider <hostingprovider@Hostings-MacBook-Air.local>
2024-06-06 13:38:18 +0530

    first

--- components/MaxWebsiteSpeed.tsx
+++ components/MaxWebsiteSpeed.tsx
@@ -3,9 +3,8 @@ import Divider from "./divider";
 
 export function MaxWebsiteSpeed() {
     return (<>
-        <Divider marginBottom="-20px" marginTop="-20px"/>
-        <div className="flex flex-col items-center justify-center bg-[#b3e0ff] p-10">
-            <div className="flex flex-row items-center justify-center w-full max-w-[1200px] gap-5">
+        <div className="flex items-center justify-center p-0 ml-5 mr-5">
+            <div className="flex flex-col lg:flex-row items-center justify-center w-full max-w-[1200px] gap-5">
                 <div className="">
                     <Image
                         src={"/Images/Logo/Comparision/LiteSpeedCache.png"}
@@ -22,12 +21,12 @@ export function MaxWebsiteSpeed() {
                     />
                 </div>
                 <div className="flex flex-col items-start justify-between gap-2">
-                    <span className="text-4xl tracking-wide text-left">Maximize Website Speed</span>
+                    <span className="lg:text-4xl ms:text-2xl tracking-wide lg:text-left text-center w-full">Maximize Website Speed</span>
                     <span>
                         <ul>
-                            <li className="ml-2 mb-2"><span className="mr-2 ml-2">✔</span> Maximize website loading speed with LiteSpeed Web Server technology.</li>
-                            <li className="ml-2 mb-2"><span className="mr-2 ml-2">✔</span> Enjoy optimized performance with advanced cache solutions.</li>
-                            <li className="ml-2 mb-2"><span className="mr-2 ml-2">✔</span> Reduce website response times up to 3 times with Object Cache for WordPress.</li>
+                            <li className="ml-2 mb-3 md:text-base text-sm"><span className="mr-2 ml-2">✔</span> Maximize website loading speed with LiteSpeed Web Server technology.</li>
+                            <li className="ml-2 mb-3 md:text-base text-sm"><span className="mr-2 ml-2">✔</span> Enjoy optimized performance with advanced cache solutions.</li>
+                            <li className="ml-2 mb-3 md:text-base text-sm"><span className="mr-2 ml-2">✔</span> Reduce website response times up to 3 times with Object Cache for WordPress.</li>
                         </ul>
                     </span>
                 </div>
